cd540857c84c767f154f157615b3dbd5f49a68fc
buyablemos <161636710+buyablemos@users.noreply.github.com>
2025-05-11 20:28:56 +0200

    code refactor, one vae deleted

--- CNNVAE.py
+++ CNNVAE.py
@@ -1,28 +1,33 @@
+import os
+
 import torch
 import torch.nn as nn
+from matplotlib import pyplot as plt
 
 # Parametry modelu
 IMG_SIZE = 128
 CHANNELS = 3
 LATENT_DIM = 256
-HIDDEN_DIM = 1024
 
 
 class CNNVAE(nn.Module):
-    def __init__(self):
+    def __init__(self, device='cpu', result_dir="results", load_pretrained=True):
         super(CNNVAE, self).__init__()
+
+        self.result_dir = result_dir
+        self.device = device
 
         # Encoder (konwolucje)
         self.encoder_conv = nn.Sequential(
             nn.Conv2d(CHANNELS, 32, kernel_size=4, stride=2, padding=1),  # [B, 32, IMG_SIZE/2, IMG_SIZE/2]
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),         # [B, 64, IMG_SIZE/4, IMG_SIZE/4]
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),  # [B, 64, IMG_SIZE/4, IMG_SIZE/4]
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),        # [B, 128, IMG_SIZE/8, IMG_SIZE/8]
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # [B, 128, IMG_SIZE/8, IMG_SIZE/8]
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),       # [B, 256, IMG_SIZE/16, IMG_SIZE/16]
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # [B, 256, IMG_SIZE/16, IMG_SIZE/16]
             nn.ReLU(),
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),       # [B, 512, IMG_SIZE/32, IMG_SIZE/32]
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),  # [B, 512, IMG_SIZE/32, IMG_SIZE/32]
             nn.ReLU()
         )
 
@@ -42,15 +47,20 @@ class CNNVAE(nn.Module):
         self.decoder_conv = nn.Sequential(
             nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),  # [B, 256, IMG_SIZE/16, IMG_SIZE/16]
             nn.ReLU(),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),   # [B, 128, IMG_SIZE/8, IMG_SIZE/8]
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # [B, 128, IMG_SIZE/8, IMG_SIZE/8]
             nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),    # [B, 64, IMG_SIZE/4, IMG_SIZE/4]
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # [B, 64, IMG_SIZE/4, IMG_SIZE/4]
             nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),     # [B, 32, IMG_SIZE/2, IMG_SIZE/2]
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # [B, 32, IMG_SIZE/2, IMG_SIZE/2]
             nn.ReLU(),
             nn.ConvTranspose2d(32, CHANNELS, kernel_size=4, stride=2, padding=1),  # [B, CHANNELS, IMG_SIZE, IMG_SIZE]
             nn.Sigmoid()  # Wartości wyjściowe w zakresie [0, 1]
         )
+
+        self.optimizer = torch.optim.Adam(self.parameters(),lr=0.001)
+
+        if load_pretrained:
+            self.load_models()
 
     def encode(self, x):
         x = self.encoder_conv(x)
@@ -73,3 +83,87 @@ class CNNVAE(nn.Module):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
+
+    def generate_from_accurate_params(self, mu, logvar, num_samples=1, device='cpu'):
+        """
+        Generuje próbki z rozkładu N(mu, exp(logvar))
+
+        Args:
+            model: VAE model
+            mu: tensor (LATENT_DIM,) - średnia
+            logvar: tensor (LATENT_DIM,) - log-wariancja
+            num_samples: int - liczba próbek do wygenerowania
+            device: str - urządzenie (cpu/cuda)
+        Returns:
+            Tensor (num_samples, CHANNELS, IMG_SIZE, IMG_SIZE)
+        """
+        self.eval()
+        with torch.no_grad():
+            mu = mu.expand(num_samples, -1).to(device)  # kształt: [num_samples, latent_dim]
+            logvar = logvar.expand(num_samples, -1).to(device)  # kształt: [num_samples, latent_dim]
+
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)  # (num_samples, LATENT_DIM)
+            z = mu + eps * std
+
+            samples = self.decode(z)  # (num_samples, CHANNELS, IMG_SIZE, IMG_SIZE)
+        return samples
+
+    def load_models(self):
+
+        model_path = os.path.join(self.result_dir, "vae_best.pth")
+
+        if os.path.exists(model_path):
+            self.load_state_dict(torch.load(model_path, map_location=self.device))
+            print("[INFO] Loaded best VAE weights.")
+        else:
+            print("[INFO] No VAE checkpoint found.")
+
+    def train_vae(self, epoch, dataloader, loss_fn):
+        self.train()
+        train_loss = 0
+        for data in dataloader:
+            data = data.to(self.device)
+            self.optimizer.zero_grad()
+            recon, mu, logvar = self.forward(data)
+            loss = loss_fn(recon_x=recon, x=data, mu=mu, logvar=logvar)
+            train_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+            #scheduler.step(loss)
+
+        train_loss /= len(dataloader.dataset)
+        print(f'Train Epoch: {epoch} | Loss: {train_loss:.4f}')
+        return train_loss
+
+    def validate(self, epoch, dataloader, loss_fn):
+        self.eval()
+        validate_loss = 0
+        with torch.no_grad():
+            for data in dataloader:
+                data = data.to(self.device)
+                recon, mu, logvar = self.forward(data)
+                validate_loss += loss_fn(recon_x=recon, x=data, mu=mu, logvar=logvar).item()
+
+        validate_loss /= len(dataloader.dataset)
+        print(f'Validate Epoch: {epoch} | Loss: {validate_loss:.4f}')
+        return validate_loss
+
+    # Wizualizacja rekonstrukcji
+    def visualize_reconstruction(self, epoch, dataloader):
+        self.eval()
+        with torch.no_grad():
+            sample = next(iter(dataloader)).to(self.device)
+
+            recon, mu, logvar = self.forward(sample)
+
+            #recon = (recon + 1) / 2
+
+            fig, axes = plt.subplots(2, 16, figsize=(32, 8))
+            for i in range(16):
+                axes[0, i].imshow((sample[i]).cpu().permute(1, 2, 0))
+                axes[0, i].axis('off')
+                axes[1, i].imshow(recon[i].cpu().permute(1, 2, 0))
+                axes[1, i].axis('off')
+            plt.savefig(f'{self.result_dir}/recon_{epoch % 100}.png')
+            plt.close()
--- VAE.py
+++ VAE.py
@@ -1,51 +1,50 @@
+import os
+
 import torch
 import torch.nn as nn
+from matplotlib import pyplot as plt
 
 # Parametry modelu
 IMG_SIZE = 128
 CHANNELS = 3
-LATENT_DIM = 256
-HIDDEN_DIM = 1024
+LATENT_DIM = 64
+HIDDEN_DIM = 512
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self,device='cpu',result_dir="results", load_pretrained=True):
         super(VAE, self).__init__()
         input_dim = IMG_SIZE * IMG_SIZE * CHANNELS
+
+        self.result_dir = result_dir
+        self.device=device
 
         # Encoder
         self.encoder = nn.Sequential(
             nn.Linear(input_dim, HIDDEN_DIM),
             nn.ReLU(),
             nn.Dropout(p=0.3),
-            nn.Linear(HIDDEN_DIM, int(HIDDEN_DIM * 0.85)),
+            nn.Linear(HIDDEN_DIM, int(HIDDEN_DIM * 0.7)),
             nn.ReLU(),
             nn.Dropout(p=0.3),
-            nn.Linear(int(HIDDEN_DIM * 0.85), int(HIDDEN_DIM * 0.65)),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(int(HIDDEN_DIM * 0.65), HIDDEN_DIM // 2),
+            nn.Linear(int(HIDDEN_DIM * 0.7), HIDDEN_DIM // 2),
             nn.ReLU(),
         )
 
         self.fc_mu = nn.Linear(HIDDEN_DIM // 2, LATENT_DIM)
         self.fc_var = nn.Linear(HIDDEN_DIM // 2, LATENT_DIM)
 
-        # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(LATENT_DIM, HIDDEN_DIM // 2),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(HIDDEN_DIM // 2, int(HIDDEN_DIM * 0.65)),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(int(HIDDEN_DIM * 0.65), int(HIDDEN_DIM * 0.85)),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(int(HIDDEN_DIM * 0.85), HIDDEN_DIM),
+            nn.Linear(LATENT_DIM, HIDDEN_DIM),
             nn.ReLU(),
             nn.Linear(HIDDEN_DIM, input_dim),
-            nn.Tanh()
+            nn.Sigmoid()
         )
+
+        self.optimizer = torch.optim.Adam(self.parameters(),lr=0.001)
+        #self.scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1, patience=10, verbose=True)
+
+        if load_pretrained:
+            self.load_models()
 
 
     def encode(self, x):
@@ -65,3 +64,94 @@ class VAE(nn.Module):
         mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
+
+    def generate_from_accurate_params(self, mu, logvar, num_samples=1, device='cpu'):
+        """
+        Generuje próbki z rozkładu N(mu, exp(logvar))
+
+        Args:
+            model: VAE model
+            mu: tensor (LATENT_DIM,) - średnia
+            logvar: tensor (LATENT_DIM,) - log-wariancja
+            num_samples: int - liczba próbek do wygenerowania
+            device: str - urządzenie (cpu/cuda)
+        Returns:
+            Tensor (num_samples, CHANNELS, IMG_SIZE, IMG_SIZE)
+        """
+        self.eval()
+        with torch.no_grad():
+
+            mu = mu.expand(num_samples, -1).to(device)          # kształt: [num_samples, latent_dim]
+            logvar = logvar.expand(num_samples, -1).to(device)  # kształt: [num_samples, latent_dim]
+
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)  # (num_samples, LATENT_DIM)
+            z = mu + eps * std
+
+            samples = self.decode(z)    # (num_samples, CHANNELS, IMG_SIZE, IMG_SIZE)
+        return samples
+
+    def load_models(self):
+
+        model_path = os.path.join(self.result_dir, "vae_best.pth")
+
+        if os.path.exists(model_path):
+            self.load_state_dict(torch.load(model_path, map_location=self.device))
+            print("[INFO] Loaded best VAE weights.")
+        else:
+            print("[INFO] No VAE checkpoint found.")
+
+
+    # Trenowanie
+    def train_vae(self, epoch, dataloader, loss_fn):
+        self.train()
+        train_loss = 0
+        for data in dataloader:
+            data = data.to(self.device)
+            self.optimizer.zero_grad()
+            recon ,mu,logvar = self.forward(data)
+            loss = loss_fn(recon_x=recon, x=data,mu=mu,logvar=logvar)
+            train_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+            #scheduler.step(loss)
+
+
+        train_loss /= len(dataloader.dataset)
+        print(f'Train Epoch: {epoch} | Loss: {train_loss:.4f}')
+        return train_loss
+
+
+
+    def validate(self, epoch,dataloader, loss_fn):
+        self.eval()
+        validate_loss = 0
+        with torch.no_grad():
+            for data in dataloader:
+                data = data.to(self.device)
+                recon ,mu,logvar = self.forward(data)
+                validate_loss += loss_fn(recon_x=recon, x=data,mu=mu,logvar=logvar).item()
+
+        validate_loss /= len(dataloader.dataset)
+        print(f'Validate Epoch: {epoch} | Loss: {validate_loss:.4f}')
+        return validate_loss
+
+    # Wizualizacja rekonstrukcji
+    def visualize_reconstruction(self,epoch, dataloader):
+        self.eval()
+        with torch.no_grad():
+            sample = next(iter(dataloader)).to(self.device)
+
+            recon ,mu,logvar = self.forward(sample)
+
+            #recon = (recon + 1) / 2
+
+            fig, axes = plt.subplots(2, 16, figsize=(32, 8))
+            for i in range(16):
+                axes[0, i].imshow((sample[i]).cpu().permute(1, 2, 0))
+                axes[0, i].axis('off')
+                axes[1, i].imshow(recon[i].cpu().permute(1, 2, 0))
+                axes[1, i].axis('off')
+            plt.savefig(f'{self.result_dir}/recon_{epoch%100}.png')
+            plt.close()
+
